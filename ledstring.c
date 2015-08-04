9b0c0d9cdb18c0dbd08e37632fa096f7e34064ca
Anthony Liekens <anthony@liekens.net>
2015-08-05 01:37:16 +0200

    move example code out, some first steps in

--- ledstring.c
+++ ledstring.c
@@ -11,7 +11,7 @@ ws2811_t ledstring =
             .gpionum = GPIO_PIN,
             .count = LED_COUNT,
             .invert = 0,
-            .brightness = 255,
+            .brightness = 32,
         },
         [1] =
         {
