2f85dca3cb39e393e4ca21c0be24bf921d5c7f91
Anthony Liekens <anthony@liekens.net>
2015-08-05 17:35:35 +0200

    TRON!!!!!

--- main.cpp
+++ main.cpp
@@ -35,6 +35,8 @@ extern "C" {
 #define BUTTON_2_LEFT_GPIO 27
 #define BUTTON_2_RIGHT_GPIO 22
 
+int playerLEDs[ 3 ] = { 397, 398, 399 };
+
 void setColor( int position, ws2811_led_t color ) {
   ledstring.channel[0].leds[position] = color;
 }
@@ -43,42 +45,161 @@ ws2811_led_t getColor( int position ) {
   return ledstring.channel[0].leds[position];
 }
 
-#define SNAKES 32
-int positions[SNAKES];
-int directions[SNAKES];
-ws2811_led_t colors[SNAKES];
-
-void setup_snake(void) {
-  for( int i = 0; i < SNAKES; i++ ) {
-    positions[ i ] = rand() % LED_COUNT;
-    directions[ i ] = rand() % 6;
-    colors[ i ] = randomColor();
+void fill( ws2811_led_t color ) {
+  for( int i = 0; i < LED_COUNT; i++ ) {
+    setColor( i, color );
   }
 }
 
-void render_snake(void) {
-  for( int i = 0; i < LED_COUNT; i++ ) {
-    setColor( i, darkenColor( getColor( i ) ) );
+void fillhexagon( ws2811_led_t color ) {
+  for( int i = 0; i < LED_COUNT - 3; i++ ) {
+    setColor( i, color );
   }
-  for( int snake = 0; snake < SNAKES; snake++ ) {
-    setColor( positions[ snake ], colors[ snake ] );
-    positions[ snake ] = neighbors[ positions[ snake ] ][ directions[ snake ] ];
-    if( ( rand() % 10 == 0 ) || ( positions[ snake ] == neighbors[ positions[ snake ] ][ directions[ snake ] ] ) ){
-      if( rand() % 2 )
-        directions[ snake ] = ( directions[ snake ] + 1 ) % 6;
-      else
-        directions[ snake ] = ( directions[ snake ] + 5 ) % 6; // smart hack
+}
+
+void fillborder( ws2811_led_t color ) {
+  for( int i = LED_COUNT - 69; i < LED_COUNT - 3; i++ ) {
+    setColor( i, color );
+  }
+}
+
+#define PLAYERS 3
+int position[PLAYERS];
+bool humanplayer[PLAYERS];
+bool alive[PLAYERS];
+int direction[PLAYERS];
+ws2811_led_t colors[PLAYERS];
+
+void setup_tron(void) {
+  for( int i = 0; i < PLAYERS; i++ ) {
+    position[ i ] = 0;
+    direction[ i ] = i * 2;
+  }
+  colors[ 0 ] = color( 255, 255, 0 );
+  colors[ 1 ] = color( 0, 255, 255 );
+  colors[ 2 ] = color( 255, 0, 255 );
+}
+
+void play_tron(void) {
+  
+  int sleep = 200000;
+  
+  while( ( alive[ 0 ] && alive[ 1 ] ) || ( alive[ 1 ] && alive[ 2 ] ) || ( alive[ 0 ] && alive[ 2 ] ) ) { // play as long as 2 players are alive
+  
+    // fade out player tails
+    for( int i = 0; i < LED_COUNT; i++ ) {
+      setColor( i, darkenColor( getColor( i ) ) );
+    }
+  
+    // update players
+    for( int player = 0; player < PLAYERS; player++ ) {
+      if( alive[ player ] ) {
+
+        if( humanplayer[ player ] ) {
+
+          if( button_rising[ player * 2 ] )
+            direction[ player ] = ( direction[ player ] + 1 ) % 6;
+          if (button_rising[ player * 2 + 1 ] )
+            direction[ player ] = ( direction[ player ] + 5 ) % 6;
+
+        } else {
+
+          if( ( position[ player ] == neighbors[ position[ player ] ][ direction[ player ] ] ) )
+            if( rand() % 2 )
+              direction[ player ] = ( direction[ player ] + 1 ) % 6;
+            else
+              direction[ player ] = ( direction[ player ] + 5 ) % 6; // smart hack
+          else if( rand() % 5 == 0 )
+            if( rand() % 2 )
+              direction[ player ] = ( direction[ player ] + 1 ) % 6;
+            else
+              direction[ player ] = ( direction[ player ] + 5 ) % 6; // smart hack
+
+        }
+
+        int next_position = neighbors[ position[ player ] ][ direction[ player ] ];
+        if( getColor( next_position ) == 0 )
+          position[ player ] = neighbors[ position[ player ] ][ direction[ player ] ];
+        else {
+          alive[ player ] = false;
+          setColor( playerLEDs[ player ], 0 );
+        }
+        
+      }
+    
+    }
+    
+    print_button_states();
+    reset_button_states();
+  
+    // render players on new positions
+    for( int player = 0; player < PLAYERS; player++ ) {
+      setColor( position[ player ], colors[ player ] );
+    }
+    
+    usleep( sleep );
+    sleep *= 0.98;
+  
+  }
+  
+}
+
+void invite_players() {
+  
+  reset_button_states();
+  
+  for( int player = 0; player < PLAYERS; player++ ) {
+    humanplayer[ player ] = false;
+    alive[ player ] = true;
+    setColor( playerLEDs[ player ] , 0 );
+  }
+
+  int counter = 0;
+  while( ( ( humanplayer[ 0 ] || humanplayer[ 1 ] || humanplayer[ 2 ] ) == false ) || ( counter < 5 ) ) { // wait a bit for players to join
+    counter++;
+    
+    for( int player = 0; player < PLAYERS; player++ ) { // check whether player pressed buttons
+      if( !humanplayer[ player ] ) // only those who haven't joined yet
+        if( button_pushed[ player * 2 ] || button_pushed[ player * 2 + 1 ] ) {
+          humanplayer[ player ] = true;
+          setColor( playerLEDs[ player ], colors[ player ] );
+          counter = 0;
+        }
+    }
+    
+    reset_button_states();
+
+    usleep( 200000 );
+  }
+  
+  fillhexagon( 0 );
+  for( int player = 0; player < PLAYERS; player++ )
+    setColor( playerLEDs[ player ], colors[ player ] );
+}
+
+void announce_winner() {
+  ws2811_led_t color = 0xffffff;
+  for( int player = 0; player < 3; player++ ) {
+    if( alive[ player ] ) {
+      color = colors[ player ];
     }
   }
+  for( int i = 0; i < 3; i++ ) {
+    fillborder( color );
+    usleep(500000);
+    fillborder( 0 );
+    usleep(500000);
+  }
 }
 
-static void ctrl_c_handler(int signum) {
-  ws2811_fini(&ledstring);
-}
-
-static void setup_handlers(void) {
-  struct sigaction sa = {ctrl_c_handler};
-  sigaction(SIGKILL, &sa, NULL);
+void logic_thread() {
+  while (1) {
+    setup_tron();
+    invite_players();
+    play_tron();
+    announce_winner();
+    usleep(200000);
+  }
 }
 
 void button_thread() {
@@ -122,14 +243,13 @@ void render_thread() {
   }
 }
 
-void logic_thread() {
-  setup_snake();
-  while (1) {
-      render_snake();
-      print_button_states();
-      reset_button_states();
-      usleep(100000);
-  }
+static void ctrl_c_handler(int signum) {
+  ws2811_fini(&ledstring);
+}
+
+static void setup_handlers(void) {
+  struct sigaction sa = {ctrl_c_handler};
+  sigaction(SIGKILL, &sa, NULL);
 }
 
 int main(int argc, char *argv[]) {
