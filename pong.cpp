5c33884b040179b2e8ef96a65eb8bcc9cc9337c7
Anthony Liekens <anthony@liekens.net>
2015-09-25 17:14:20 +0200

    pong tweaking

--- pong.cpp
+++ pong.cpp
@@ -8,6 +8,8 @@
 
 #include "buffer.h"
 
+#define PLAYER_SIZE 0.2
+
 // draw circles and lines in the unit coordinate system
 
 float random_float() {
@@ -29,7 +31,7 @@ public:
   Coordinate getCoordinate();
   
   void update();
-  void render( Buffer* buffer );
+  void render( Buffer* buffer, double size = PLAYER_SIZE );
 };
 
 Player::Player() {
@@ -39,8 +41,8 @@ Player::Player( int id ) {
   alive = true;
   this->id = id;
   position = 0.5;
-  leftCoordinate = Coordinate( 1.05 * cos( ( 4 - id * 2 ) * TAU / 6 ), 1.05 * sin( ( 4 - id * 2 ) * TAU / 6 ) );
-  rightCoordinate = Coordinate( 1.05 * cos( ( 5 - id * 2 ) * TAU / 6 ), 1.05 * sin( ( 5 - id * 2 ) * TAU / 6 ) );
+  leftCoordinate = Coordinate( 1.1 * cos( ( 4 - id * 2 ) * TAU / 6 ), 1.1 * sin( ( 4 - id * 2 ) * TAU / 6 ) );
+  rightCoordinate = Coordinate( 1.1 * cos( ( 5 - id * 2 ) * TAU / 6 ), 1.1 * sin( ( 5 - id * 2 ) * TAU / 6 ) );
   leftButton = id * 2;
   rightButton = id * 2 + 1;
   switch(id) {
@@ -75,8 +77,8 @@ Coordinate Player::getCoordinate() {
   return leftCoordinate * ( 1 - position ) + rightCoordinate * position;
 }
 
-void Player::render( Buffer* buffer ) {
-  Circle circle( getCoordinate(), 0.14 );
+void Player::render( Buffer* buffer, double size ) {
+  Circle circle( getCoordinate(), size );
   circle.render( buffer, color );
 }
 
@@ -86,7 +88,7 @@ void logic_thread() {
   Line borders[ 6 ];
   for( int i = 0; i < 6; i++ ) {
     borders[ i ] = Line( 
-      Coordinate( 1.025 * cos( i * TAU / 6 ), 1.025 * sin( i * TAU / 6 ) ), 
+      Coordinate( 1.0 * cos( i * TAU / 6 ), 1.0 * sin( i * TAU / 6 ) ), 
       Vector( ( i + 2 ) * TAU / 6, 1 ) 
     );
   }
@@ -104,6 +106,18 @@ void logic_thread() {
       Coordinate( 0,0 ), 
       Vector( ( 3 + 4 * ( rand() % 3 ) ) * TAU / 12 + random_float() * 0.1 - 0.05, 0.04 ) 
     );
+    
+    // render the arena and show it without animation for half a second
+    buffer->fill( 0 );
+    for( int i = 0; i < 3; i++ ) {
+      borders[ i * 2 + 1 ].render( buffer, COLOR_WHITE );
+      players[ i ].render( buffer );
+    }
+    Circle c( ball.coordinate, 1/12.0 );
+    c.render( buffer, COLOR_YELLOW );
+    buffer->render();
+    usleep(500000); 
+    reset_button_states();
 
     while ( 
       ( players[ 0 ].alive || players[ 1 ].alive ) && 
@@ -130,29 +144,31 @@ void logic_thread() {
             players[ 1 ].alive = false;
           if( i == 0 )
             players[ 2 ].alive = false;
+          borders[ i ].render( buffer, COLOR_WHITE ); // flash wall
         }
       
         // check if we bounce into a circle
         for( int player = 0; player < 3; player++ ) 
         if( players[ player ].alive ) {
           Coordinate position = players[ player ].getCoordinate();
-          float ballwidth = 0.15;
-          if( position.distance( ball.coordinate ) < ballwidth ) {
+          if( position.distance( ball.coordinate ) < PLAYER_SIZE ) {
             // mirror the vector angle of the ball
             double circle_edge_angle = atan2( ball.coordinate.x - position.x, - ball.coordinate.y + position.y );
             double circle_norm_angle = circle_edge_angle - TAU / 4;
-            ball.coordinate.x = position.x + ballwidth * cos( circle_norm_angle );
-            ball.coordinate.y = position.y + ballwidth * sin( circle_norm_angle );
+            ball.coordinate.x = position.x + PLAYER_SIZE * cos( circle_norm_angle );
+            ball.coordinate.y = position.y + PLAYER_SIZE * sin( circle_norm_angle );
             ball.vector.angle = 2 * circle_edge_angle - ball.vector.angle;
             if( button_state[ players[ player ].leftButton ] )
               spin -= 0.005;
             if( button_state[ players[ player ].rightButton ] )
               spin += 0.005;
+            players[ player ].render( buffer, PLAYER_SIZE * 1.5 ); // flash player
           }
         }
         
       }
       
+      // collect button states, move players and reset button states ASAP
       for( int player = 0; player < 3; player ++ ) players[ player ].update(); // do not put these for loops in 1 loop
       reset_button_states();
 
@@ -177,7 +193,7 @@ void logic_thread() {
       c.render( buffer, COLOR_YELLOW );
 
       // increase ball speed
-      ball.vector.norm = 1.001 * ball.vector.norm;
+      ball.vector.norm = 1.002 * ball.vector.norm;
       
       // use spin to change directions
       ball.vector.angle += spin;
@@ -190,12 +206,15 @@ void logic_thread() {
     
     int winner = 0;
     for( int player = 0; player < 3 ; player++ )if( players[ player ].alive ) { winner = player; }
-    for( float i = 0.0; i < 1.0; i += 0.01 ) {
-      Circle circle( players[ winner ].getCoordinate(), i );
-      circle.render( buffer, players[ winner ].color );
+    for( int i = 0; i < 10; i++ ) {
+      Circle circle( players[ winner ].getCoordinate(), PLAYER_SIZE );
+      if( i % 2 )
+        circle.render( buffer, players[ winner ].color );
+      else
+        circle.render( buffer, 0 );
       buffer->render();
-      usleep(10000); // 50 per second
-    }
+      usleep(200000);
+    }    
     
   }
   
