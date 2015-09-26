cfe946722bbd1b404caa6450306eca0fbf88f61a
Anthony Liekens <anthony@liekens.net>
2015-09-26 18:44:55 +0200

    SDL graphics and faked gamma error

--- Makefile
+++ Makefile
@@ -11,19 +11,26 @@ HARDWARE_OBJECTS = hexxx.o $(COMMON_OBJECTS)
 SIMULATOR_OBJECTS = hexxx_simulator.o $(COMMON_OBJECTS)
 
 HARDWARE_LIBS = ws2811/libws2811.a gpio/gpio.a -lX11
+SIMULATOR_LIBS=`sdl2-config --libs` -lSDL2_gfx
 
-#CPPFLAGS = -std=c++0x -O2
-#CFLAGS = -O2
+#CPPFLAGS = -std=c++0x -O3
+#CFLAGS = -O3
 
 $(HARDWARE_LIBS): force_look
 	cd ws2811 ; make
 	cd gpio ; make
 
+hexxx_simulator.o: hexxx_simulator.cpp
+	g++ `sdl2-config --cflags` -O3 -c -o hexxx_simulator.o hexxx_simulator.cpp
+
 example: $(HARDWARE_LIBS) $(HARDWARE_OBJECTS) example.o
 	g++ $(CPPFLAGS) -o example example.o $(HARDWARE_OBJECTS) $(HARDWARE_LIBS) -pthread
 
-example_simulator: $(SIMULATOR_LIBS) $(SIMULATOR_OBJECTS) example.o
+example_simulator: $(SIMULATOR_OBJECTS) example.o
 	g++ $(CPPFLAGS) -o example_simulator example.o $(SIMULATOR_OBJECTS) $(SIMULATOR_LIBS) -lpthread
+
+cube_simulator: $(SIMULATOR_OBJECTS) cube.o
+	g++ $(CPPFLAGS) -o cube_simulator cube.o $(SIMULATOR_OBJECTS) $(SIMULATOR_LIBS) -lpthread
 
 tron: $(HARDWARE_LIBS) $(HARDWARE_OBJECTS) tron.o
 	g++ $(CPPFLAGS) -o tron tron.o $(HARDWARE_OBJECTS) $(HARDWARE_LIBS) -pthread
@@ -48,6 +55,9 @@ vectors: $(HARDWARE_LIBS) $(HARDWARE_OBJECTS) vectors.o
 
 pong: $(HARDWARE_LIBS) $(HARDWARE_OBJECTS) pong.o
 	g++ $(CPPFLAGS) -o pong pong.o $(HARDWARE_OBJECTS) $(HARDWARE_LIBS) -pthread
+
+pong_simulator: $(SIMULATOR_OBJECTS) pong.o
+	g++ $(CPPFLAGS) -o pong_simulator pong.o $(SIMULATOR_OBJECTS) $(SIMULATOR_LIBS) -lpthread
 
 clean:
 	@rm -vf *.o *.a $(TARGETS)
--- hexxx_simulator.cpp
+++ hexxx_simulator.cpp
@@ -1,3 +1,11 @@
+// SIMULATOR SPECIFIC INCLUDES
+
+#include "SDL.h"
+#include "SDL2_gfxPrimitives.h"
+#include "coordinates.h"
+
+// OTHER INCLUDES
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,6 +27,7 @@
 #include "hexxx.h"
 
 #define LED_COUNT                                400
+#define HEXAGON_LED_COUNT                        397
 ws2811_led_t simulated_ledstring[ LED_COUNT ];
 
 #define BUTTON_0_LEFT_GPIO 2
@@ -74,12 +83,47 @@ void button_thread() {
   }
 }
 
+uint32_t SDLcolor(uint8_t r, uint8_t g, uint8_t b, uint8_t a ) {
+  return ((uint32_t)r << 24) | ((uint32_t)g <<  16) | ((uint32_t)b <<  8) | a;
+}
+
 void render_thread() {
   
+	/* Init SDL */
+	if( SDL_Init( SDL_INIT_TIMER | SDL_INIT_VIDEO ) < 0 ) {
+		std::cerr << "Couldn't initialize SDL: " << SDL_GetError() << std::endl;
+		exit( 1 );
+	}
+	atexit( SDL_Quit );
+  
+	/* Initialize the display */
+	SDL_Window *sdlWindow = SDL_CreateWindow("HEXXX simulator", // SDL2
+	                          SDL_WINDOWPOS_CENTERED,
+	                          SDL_WINDOWPOS_CENTERED,
+	                          400, 400,
+	                          SDL_WINDOW_OPENGL);
+	SDL_Renderer *sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, 0);
+
   while (1) {
-//      ws2811_render(&ledstring);
-      
-      usleep(15000); // slow down to about 50FPS on a raspberry pi 2
+
+		SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
+		SDL_RenderClear(sdlRenderer);
+		for( int i = 0; i < HEXAGON_LED_COUNT; i++ ) {
+      // we'll simulate gamma error of ws2811 LEDs by applying "gamma error" functions which are inverted of gamma correction
+      int red = applyGammaError( getRed( simulated_ledstring[ i ] ) );
+      int green = applyGammaError( getGreen( simulated_ledstring[ i ] ) );
+      int blue = applyGammaError( getBlue( simulated_ledstring[ i ] ) );
+			filledCircleRGBA( 
+        sdlRenderer, 
+        200 - 180 * led2unitx(i), 
+        200 - 180 * led2unity(i),
+        5, // radius
+        red, green, blue, 255 
+      );
+		}
+		SDL_RenderPresent(sdlRenderer);
+		
+    usleep(15000); // slow down to about 50FPS on a raspberry pi 2
   }
 }
 
@@ -97,8 +141,6 @@ int main(int argc, char *argv[]) {
   t1.join();
   t2.join();
   t3.join();
-
-//  ws2811_fini(&ledstring);
 
   return ret;
   
