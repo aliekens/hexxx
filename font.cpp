57b0147a45aa2af64ff2f35ec2877f61c805c57c
Anthony Liekens <anthony@liekens.net>
2015-09-28 23:58:07 +0200

    font overhaul

--- font.cpp
+++ font.cpp
@@ -1,36 +1,52 @@
 #include "font.h"
 
-#include <string>
+#include <iostream>
+#include <fstream>
 
-#include "coordinates.h"
 #include "hexxx.h"
+#include "coordinates.h"
+#include "buffer.h"
 
-uint32_t font[5][6] = {
-  { 0b111110, 0b011000, 0b111100, 0b010000, 0b110001, 0b111111 }, // E
-  { 0b110011, 0b010001, 0b111111, 0b010001, 0b110011, 0b110011 }, // H
-  { 0b111011, 0b001110, 0b001100, 0b001110, 0b010110, 0b110011 }, // X
-  { 0b000000, 0b001110, 0b011110, 0b001110, 0b001100, 0b000000 },  // diamond
-  { 0b000000, 0b000000, 0b000000, 0b000000, 0b000000, 0b000000 }  // space
-};
+Font::Font( const std::string& filename ) {
+  std::ifstream fontfile( filename );
+  if (fontfile.is_open()) {
+    fontfile >> sizex;
+    fontfile >> sizey;
+    std::string line;
+    getline (fontfile,line); // skip first line
+    while ( getline (fontfile,line) ) {
+      char letter = line[ 0 ];
+      for( int y = 0; y < sizey; y++ ) {
+        std::vector< bool > row;
+        for( int x = 0; x < sizex; x++ ) {
+          row.push_back( line[ y * sizex + x + 1 ] == '1' );
+        }
+        font[ letter ].push_back( row );
+      }
+    }
+    fontfile.close();
+  } else 
+    std::cerr << "Unable to open font file " << filename << std::endl; 
+}
 
-std::string characters = "EHXx ";
-
-void printCharacter( int skewedx, int skewedy, char c, ws2811_led_t color ) {
-  int fontposition = characters.find( c );
-  if( fontposition != std::string::npos ) {
-    for( int x = 0; x < 6; x++ ) {
-      for( int y = 0; y < 6; y++ ) {
-        if( ( font[ fontposition ][ 5 - y ] >> ( 5 - x ) ) & 0x01 )
-          setColor( skewed2led( skewedx + x - ( y / 2 ), skewedy + y ), color );
+void Font::printCharacter( Buffer* buffer, int skewedx, int skewedy, char c, ws2811_led_t color ) {
+  if ( font.find( c ) != font.end() ) {
+    std::vector< std::vector< bool > > character = font[ c ];
+    for( int x = 0; x < sizex; x++ ) {
+      for( int y = 0; y < sizey; y++ ) {
+        if( character[ y ][ x ] ) {
+          int led = skewed2led( skewedx + x, skewedy + sizey - y );
+          buffer->setPixel( led, color );
+        }
       }
     }
   }
 }
 
-void printString( int skewedx, int skewedy, std::string str, ws2811_led_t color ) {
+void Font::printString( Buffer* buffer, int skewedx, int skewedy, std::string str, ws2811_led_t color ) {
   int counter = 0;
   for(char& c : str) {
-    printCharacter( skewedx + counter * 7, skewedy, c, color );
+    printCharacter( buffer, skewedx + counter * ( sizex + 1 ), skewedy, c, color );
     counter++;
   }
 }
